f4902e35c81248229f87238926ca8a584a2f4693
Ben110203 <35886315+Ben110203@users.noreply.github.com>
2018-03-18 21:08:52 +0800

    324

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ URLdict = {"abc":"www.google.com.tw"}
 def reponse(path):
     return  flask.redirect(getURL(path))
 
-def getURL(url):
-    if URLdict.get(url,None)!= None:
-        return flask.redirect(URLdict[url])
-    else:
-        return None
+def getURL(url)
 
+    try:
+
+        if URLdict.get(url,None)!= None:
+            return flask.redirect(URLdict[url])
+        else:
+            return None
+    except:
+        return "ERROR"
 
 if __name__ == "__main__":
     app.run()
